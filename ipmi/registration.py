d0c8e6654299ea22f1402827af7d54795015a62e
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-05 23:19:00 +0000

    Add register() function

--- ipmi/registration.py
+++ ipmi/registration.py
@@ -1,6 +1,7 @@
-import tempfile
 
 from nipype.interfaces import niftyreg
+
+from .path import ensure_dir, get_temp_path
 
 NN = 'NN'
 LIN = 'LIN'
@@ -8,7 +9,19 @@ CUB = 'CUB'
 SINC = 'SINC'
 
 def register(ref_path, flo_path, trsf_path=None, res_path=None):
-    return
+    if trsf_path is None:
+        trsf_path = get_temp_path('.txt')
+    if res_path is None:
+        res_path = get_temp_path('.nii.gz')
+    ensure_dir(res_path)
+    ensure_dir(trsf_path)
+    aladin = niftyreg.RegAladin()
+    aladin.inputs.ref_file = ref_path
+    aladin.inputs.flo_file = flo_path
+    aladin.inputs.aff_file = trsf_path
+    aladin.inputs.res_file = res_path
+    aladin.run()
+    return aladin
 
 
 def resample(flo_path, ref_path, trsf_path, res_path, interpolation='SINC'):
