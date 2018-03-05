14c62aec7a3e269b84a5734fa61232ebcc286acd
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-05 21:39:50 +0000

    Add ensure_dir()

--- ipmi/path.py
+++ ipmi/path.py
@@ -4,6 +4,22 @@ from . import constants as const
 
 segmented_images_dir = const.ROOT_DIR / 'Segmented_images'
 unsegmented_images_dir = const.ROOT_DIR / 'Unsegmented_images'
+output_dir = const.ROOT_DIR / 'output'
+registration_dir = output_dir / 'registration'
+template_path = registration_dir / 'template.nii.gz'
+priors_csf_path = registration_dir / 'csf_priors.nii.gz'
+priors_gm_path = registration_dir / 'gm_priors.nii.gz'
+priors_wm_path = registration_dir / 'wm_priors.nii.gz'
+
+
+def ensure_dir(path):
+    if path.exists():
+        return
+    is_dir = not path.suffixes
+    if is_dir:
+        path.mkdir(parents=True)
+    else:
+        path.parents[0].mkdir(parents=True)
 
 def get_unsegmented_images_and_ages():
     images_paths = sorted(unsegmented_images_dir.glob('*.nii.gz'))
