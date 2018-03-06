f8e2ce5515d22146e017caaab4f1d80482d3b7e7
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-06 00:25:48 +0000

    Fix floating labels paths

--- scripts/register_groupwise.py
+++ scripts/register_groupwise.py
@@ -10,16 +10,21 @@ class RegisterGroupwisePipeline:
 
     def __init__(self):
         self.images_paths, self.labels_paths = get_segmented_images_and_labels()
+
         self.ref_image_path = self.images_paths[REFERENCE_INDEX]
         self.ref_labels_path = self.labels_paths[REFERENCE_INDEX]
-        self.flo_paths = self.images_paths[:]
-        self.flo_paths.remove(self.ref_image_path)
+
+        self.flo_images_paths = self.images_paths[:]
+        self.flo_images_paths.remove(self.ref_image_path)
+
+        self.flo_labels_paths = self.labels_paths[:]
+        self.flo_labels_paths.remove(self.ref_labels_path)
 
         self.aff_paths = []
         self.res_images_paths = []
         self.res_labels_paths = []
         ref_name = self.ref_image_path.name.replace('.nii.gz', '')
-        for flo_path in self.flo_paths:
+        for flo_path in self.flo_images_paths:
             flo_name = flo_path.name.replace('.nii.gz', '')
 
             aff_name = f'{flo_name}_to_{ref_name}.txt'
@@ -36,33 +41,34 @@ class RegisterGroupwisePipeline:
 
 
     def register_all(self):
-        for flo_path, aff_path in zip(self.flo_paths, self.aff_paths):
+        for flo_path, aff_path in zip(self.flo_images_paths, self.aff_paths):
             reg.register(self.ref_image_path, flo_path, trsf_path=aff_path)
 
 
     def resample_all(self):
         print('here')
-        zipped = zip(self.flo_paths,
+        zipped = zip(self.flo_images_paths,
+                     self.flo_labels_paths,
                      self.aff_paths,
                      self.res_images_paths,
                      self.res_labels_paths)
-        for flo_path, aff_path, res_image_path, res_labels_path in zipped:
-            reg.resample(flo_path, self.ref_image_path, aff_path,
-                         res_image_path, interpolation=reg.SINC)
-            reg.resample(flo_path, self.ref_image_path, aff_path,
-                         res_labels_path, interpolation=reg.NN)
+        for flo, labels, aff, res_image, res_labels in zipped:
+            # reg.resample(flo, self.ref_image_path, aff,
+            #              res_image, interpolation=reg.SINC)
+            reg.resample(labels, self.ref_image_path, aff,
+                         res_labels, interpolation=reg.NN)
 
 
     def compute_means(self):
         images_paths = [self.ref_image_path] + self.res_images_paths
         labels_paths = [self.ref_labels_path] + self.res_labels_paths
-        reg.compute_mean_image(images_paths, path.template_path)
+        # reg.compute_mean_image(images_paths, path.template_path)
         reg.compute_mean_labels(labels_paths, path.priors_map)
 
 
 def main():
     pipeline = RegisterGroupwisePipeline()
-    pipeline.register_all()
+    # pipeline.register_all()
     pipeline.resample_all()
     pipeline.compute_means()
 
