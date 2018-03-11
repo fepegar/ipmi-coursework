7f75cbc7996e397239bee237ca03f4b02acc68a2
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-11 12:04:27 +0000

    Add label for NaNs

--- scripts/make_colormap.py
+++ scripts/make_colormap.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 import numpy as np
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 project_dir = Path(__file__).parents[1]
@@ -13,5 +15,6 @@ lines = []
 for i, alpha in enumerate(alphas):
     r, g, b, a = plt.cm.RdBu(1 - i / N, alpha=alpha, bytes=True)
     lines.append(f'{i} {i} {r} {g} {b} {int(alpha * 255)}')
+lines.append('256 NaN 0 255 0 255')
 
 output_path.write_text('\n'.join(lines))
