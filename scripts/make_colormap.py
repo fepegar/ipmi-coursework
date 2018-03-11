e97b7e2fd8cfb817156b3f761855331bc5cb065e
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-11 01:32:22 +0000

    Add jacobian color table and script to generate it

---
+++ scripts/make_colormap.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+project_dir = Path(__file__).parents[1]
+output_path = Path(project_dir, 'BlueRedDivergingAlpha.ctbl')
+
+N = 256
+alphas = np.linspace(1, 0, N // 2).tolist()
+alphas += list(reversed(alphas))
+lines = []
+for i, alpha in enumerate(alphas):
+    r, g, b, a = plt.cm.RdBu(1 - i / N, alpha=alpha, bytes=True)
+    lines.append(f'{i} {i} {r} {g} {b} {int(alpha * 255)}')
+
+output_path.write_text('\n'.join(lines))
