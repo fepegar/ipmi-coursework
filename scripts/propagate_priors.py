3e0dfd913275903a01ae49f4e2df65905ec7a17a
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-11 22:25:29 +0000

    Add method to generate de Dice scores report

--- scripts/propagate_priors.py
+++ scripts/propagate_priors.py
@@ -7,9 +7,13 @@ each of the unsegmented images and applying the transformations to the priors
 images.
 """
 
+import csv
 import multiprocessing as mp
 
+import numpy as np
+
 import ipmi
+from ipmi import path
 from ipmi import template
 from ipmi import registration as reg
 
@@ -22,7 +26,7 @@ class PropagatePriorsPipeline:
         self.subjects.extend(ipmi.get_unsegmented_subjects())
 
 
-    def register_template_to_subjects(self, free_form=True):
+    def register_template_to_subjects(self, free_form=True, force=False):
         """Register the template to each unsegmented image"""
         flo_path = template.get_final_template().template_image_path
         processes_affine = []
@@ -32,7 +36,7 @@ class PropagatePriorsPipeline:
             cpp_path = subject.template_to_t1_affine_ff_path
             ref_path = subject.t1_path
 
-            if not aff_path.is_file():
+            if not aff_path.is_file() or force:
                 # Linear
                 args = ref_path, flo_path
                 kwargs = {'trsf_path': aff_path}
@@ -41,10 +45,14 @@ class PropagatePriorsPipeline:
                 process.start()
                 processes_affine.append(process)
 
-            if free_form and not cpp_path.is_file():
+            if (free_form and not cpp_path.is_file()) or force:
+                res_path = subject.template_on_t1_affine_ff_path
                 # Free-form
                 args = ref_path, flo_path
-                kwargs = {'trsf_path': cpp_path, 'init_trsf_path': aff_path}
+                kwargs = {'trsf_path': cpp_path,
+                          'init_trsf_path': aff_path,
+                          'res_path': res_path,
+                         }
                 process = mp.Process(target=reg.register_free_form,
                                      args=args, kwargs=kwargs)
                 process.start()
@@ -63,15 +71,61 @@ class PropagatePriorsPipeline:
                 process.join()
 
 
-    def propagate_priors(self):
+    def propagate_priors(self, force=False):
         for subject in self.subjects:
             subject.propagate_priors()
 
 
+    def segment(self, force=False):
+        for subject in self.subjects:
+            print('Segmenting', subject)
+            subject.segment()
+
+
+    def dice(self):
+        subjects = ipmi.get_segmented_subjects()
+        scores_tuples = [subject.dice_scores() for subject in subjects]
+
+        csfs = []
+        gms = []
+        wms = []
+        for scores in scores_tuples:
+            csfs.append(scores.csf)
+            gms.append(scores.grey_matter)
+            wms.append(scores.white_matter)
+        scores_array = np.column_stack([csfs, gms, wms])
+        scores_array = np.hstack([scores_array,
+                                  scores_array.mean(axis=1).reshape(-1, 1)])
+
+        with open(path.dice_report_path, 'w') as csvfile:
+            fieldnames = 'Subject', 'CSF', 'Grey matter', 'White matter', 'Mean'
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for subject, (csf, gm, wm, mean) in zip(subjects, scores_array):
+                writer.writerow({'Subject': subject.id,
+                                 'CSF': csf,
+                                 'Grey matter': gm,
+                                 'White matter': wm,
+                                 'Mean': mean,
+                                })
+            means = scores_array.mean(axis=0)
+            csf, gm, wm, mean = means
+            writer.writerow({'Subject': 'Mean',
+                             'CSF': csf,
+                             'Grey matter': gm,
+                             'White matter': wm,
+                             'Mean': mean,
+                            })
+
+
+
 def main():
+    force = True
     pipeline = PropagatePriorsPipeline()
-    pipeline.register_template_to_subjects()
-    pipeline.propagate_priors()
+    # pipeline.register_template_to_subjects(force=force)
+    pipeline.propagate_priors(force=force)
+    pipeline.segment(force=force)
+
 
 if __name__ == '__main__':
     main()
