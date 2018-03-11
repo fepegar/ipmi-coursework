d396e0a8d3fb64b450306f3aac903a987c72d1c6
Fernando Perez-Garcia <fepegar@gmail.com>
2018-03-11 17:47:59 +0000

    Add Dice method and refactor paths

--- ipmi/subject.py
+++ ipmi/subject.py
@@ -1,4 +1,7 @@
 import re
+from collections import namedtuple
+
+import nibabel as nib
 
 from . import path
 from . import constants as const
@@ -20,91 +23,14 @@ PRIORS = '_priors'
 class Subject:
     #pylint: disable=E1101
 
-    def __init__(self, subject_id):
-        self.id = subject_id
+    def __init__(self):
 
-
-    def __repr__(self):
-        return f'Subject {self.id}'
-
-
-    def get_t1_path(self):
-        return self.dir / (self.id + T1 + NII_EXT)
-
-
-    def import_t1(self, t1_path):
-        if not self.t1_path.is_file():
-            path.ensure_dir(self.t1_path)
-            self.t1_path.symlink_to(t1_path)
-
-
-    def get_affine_to_template_path(self, template):
-        aff_path = self.transforms_dir / (
-            self.id + T1 + f'_to_{template.id}' + AFFINE_EXT)
-        return aff_path
-
-
-    def get_image_on_template_path(self, template):
-        res_path = self.resampled_dir / (
-            self.id + T1 + f'_on_{template.id}' + NII_EXT)
-        return res_path
-
-
-    def get_label_map_on_template_path(self, template):
-        res_path = self.resampled_dir / (
-            self.id + LABEL_MAP + f'_on_{template.id}' + NII_EXT)
-        return res_path
-
-
-
-class SegmentedSubject(Subject):
-
-    def __init__(self, subject_id, t1_path=None, label_map_path=None):
-        super().__init__(subject_id)
-        self.dir = path.segmented_subjects_dir / self.id
         self.transforms_dir = self.dir / 'transforms'
         self.resampled_dir = self.dir / 'resampled'
-
-        self.t1_path = self.get_t1_path()
-        self.label_map_path = self.dir / (self.id + LABEL_MAP + NII_EXT)
-        self.brain_mask_path = self.dir / (self.id + '_brain_mask' + NII_EXT)
-        self.t1_masked_path = self.dir / (self.id + T1 + '_masked' + NII_EXT)
-
-        if t1_path is not None:
-            self.import_t1(t1_path)
-
-        if label_map_path is not None:
-            if not self.label_map_path.is_file():
-                path.ensure_dir(self.label_map_path)
-                self.label_map_path.symlink_to(label_map_path)
-
-
-    def make_brain_mask(self, force=False):
-        if not self.brain_mask_path.is_file() or force:
-            seg.get_brain_mask_from_label_map(self.label_map_path,
-                                              self.brain_mask_path)
-
-
-    def mask_t1(self, force=False):
-        if not self.t1_masked_path.is_file() or force:
-            seg.mask(self.t1_path, self.brain_mask_path, self.t1_masked_path)
-
-
-
-class UnsegmentedSubject(Subject):
-
-    def __init__(self, subject_id, t1_age_path=None):
-        super().__init__(subject_id)
-        self.dir = path.unsegmented_subjects_dir / self.id
-        self.transforms_dir = self.dir / 'transforms'
         self.priors_dir = self.dir / 'priors'
         self.segmentation_dir = self.dir / 'segmentation'
 
-        self.t1_path = self.get_t1_path()
-        self.age_path = self.dir / (self.id + AGE + TXT_EXT)
-
-        if t1_age_path is not None:
-            self.import_t1_id_and_age(t1_age_path)
+        self.t1_path = self.dir / (self.id + T1 + NII_EXT)
 
         ## Registration ##
         self.template_to_t1_affine_path = self.transforms_dir / (
@@ -144,6 +70,101 @@ class UnsegmentedSubject(Subject):
         }
 
         self.segmentation_costs_path = self.segmentation_dir / 'costs.npy'
+
+
+    def __repr__(self):
+        return f'Subject {self.id}'
+
+
+
+
+
+    def get_affine_to_template_path(self, template):
+        aff_path = self.transforms_dir / (
+            self.id + T1 + f'_to_{template.id}' + AFFINE_EXT)
+        return aff_path
+
+
+    def get_image_on_template_path(self, template):
+        res_path = self.resampled_dir / (
+            self.id + T1 + f'_on_{template.id}' + NII_EXT)
+        return res_path
+
+
+    def get_label_map_on_template_path(self, template):
+        res_path = self.resampled_dir / (
+            self.id + LABEL_MAP + f'_on_{template.id}' + NII_EXT)
+        return res_path
+
+
+
+class SegmentedSubject(Subject):
+
+    def __init__(self, subject_id, t1_path=None, label_map_path=None):
+        self.id = subject_id
+        self.dir = path.segmented_subjects_dir / self.id
+        super().__init__()
+
+
+        self.t1_path = self.get_t1_path()
+        self.label_map_path = self.dir / (self.id + LABEL_MAP + NII_EXT)
+        self.brain_mask_path = self.dir / (self.id + '_brain_mask' + NII_EXT)
+        self.t1_masked_path = self.dir / (self.id + T1 + '_masked' + NII_EXT)
+
+        if t1_path is not None:
+            self.import_t1(t1_path)
+
+        if label_map_path is not None:
+            if not self.label_map_path.is_file():
+                path.ensure_dir(self.label_map_path)
+                self.label_map_path.symlink_to(label_map_path)
+
+
+    def import_t1(self, t1_path):
+        if not self.t1_path.is_file():
+            path.ensure_dir(self.t1_path)
+            self.t1_path.symlink_to(t1_path)
+
+
+    def make_brain_mask(self, force=False):
+        if not self.brain_mask_path.is_file() or force:
+            seg.get_brain_mask_from_label_map(self.label_map_path,
+                                              self.brain_mask_path)
+
+
+    def mask_t1(self, force=False):
+        if not self.t1_masked_path.is_file() or force:
+            seg.mask(self.t1_path, self.brain_mask_path, self.t1_masked_path)
+
+
+    def dice_scores(self):
+        DiceScores = namedtuple('DiceScores',
+                                ['gray_matter', 'white_matter', 'csf'])
+        manual_segmentation = nib.load(str(self.label_map_path)).get_data()
+        scores_list = []
+        for k, image_path in self.segmentation_paths_map.items():
+            if k == const.BACKGROUND:  # ignore background
+                continue
+            tissue_manual = manual_segmentation == k
+            tissue_automatic = nib.load(str(image_path)).get_data()
+            score = seg.dice(tissue_manual, tissue_automatic)
+            scores_list.append(score)
+        gm, wm, csf = scores_list
+        scores = DiceScores(gray_matter=gm, white_matter=wm, csf=csf)
+        return scores
+
+
+
+class UnsegmentedSubject(Subject):
+
+    def __init__(self, subject_id, t1_age_path=None):
+        self.id = subject_id
+        self.dir = path.unsegmented_subjects_dir / self.id
+        super().__init__()
+        self.age_path = self.dir / (self.id + AGE + TXT_EXT)
+
+        if t1_age_path is not None:
+            self.import_t1_id_and_age(t1_age_path)
 
 
     def __repr__(self):
