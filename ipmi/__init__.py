fe06677929c3cfd0f37466f9e44f5216b8aa26b5
Fernando <fepegar@gmail.com>
2018-03-29 15:04:25 +0100

    Return sorted list of subjects

--- ipmi/__init__.py
+++ ipmi/__init__.py
@@ -7,13 +7,13 @@ from .subject import SegmentedSubject, UnsegmentedSubject
 
 
 def get_segmented_subjects():
-    dirs = path.segmented_subjects_dir.glob('?')
+    dirs = sorted(path.segmented_subjects_dir.glob('?'))
     subjects = [SegmentedSubject(d.name) for d in dirs]
     return subjects
 
 
 def get_unsegmented_subjects():
-    dirs = path.unsegmented_subjects_dir.glob('??')
+    dirs = sorted(path.unsegmented_subjects_dir.glob('??'))
     subjects = [UnsegmentedSubject(d.name) for d in dirs]
     return subjects
 
