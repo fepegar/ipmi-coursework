337b35b3874a9ed2d69513077b30ebab18e108e1
Fernando <fepegar@gmail.com>
2018-04-04 17:41:16 +0100

    Modify order of imports and settings

--- scripts/report/dices_bars.py
+++ scripts/report/dices_bars.py
@@ -1,14 +1,17 @@
 import pathlib
 import pandas
-import seaborn as sns
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
-from ipmi.path import dice_report_pandas_path
+import seaborn as sns
 
-repo_dir = pathlib.Path(__file__).parents[2]
-figure_path = repo_dir / 'latex' / 'figures' / 'dices_bars.png'
+from ipmi.path import dice_report_pandas_path
 
 sns.set()
 sns.set_context('paper')
+
+repo_dir = pathlib.Path(__file__).parents[2]
+figure_path = repo_dir / 'latex' / 'figures' / 'dices_bars.png'
 
 dices = pandas.read_csv(dice_report_pandas_path)
 grid = sns.factorplot(x='Subject ID', y='Dice score', data=dices, hue='Tissue',
