a0c01dd6bb02a104dba48b1e216d69ec1a08f5ec
Fernando <fepegar@gmail.com>
2018-04-05 14:47:24 +0100

    Remove my name from references and setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='ipmi',
       version='0.1.0',
-      author='Fernando Perez-Garcia',
-      author_email='fernando.perezgarcia.17@ucl.ac.uk',
+      author='17113498',
       packages=find_packages(exclude=['*tests']),
       install_requires=[
           'nibabel',
